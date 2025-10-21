c8d483af196b87fce583a9ec74057e90b553a9e8
ashrafkahoush-ux <ashraf.kahoush@gmail.com>
2025-10-22 00:58:34 +0300

    fix: add outputFileTracingRoot to eliminate workspace warning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
   env: {
     NEXT_PUBLIC_DEV_SERVER: 'http://192.168.1.6:3002',
   },
